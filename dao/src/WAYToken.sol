660633b6f40052aba5f88d884b804cce974e14d9
박진환 <parkjinhwan@bagjinhwan-ui-MacBookPro.local>
2025-02-26 22:24:00 +0900

    feat: WAYToken.sol

---
+++ dao/src/WAYToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+
+contract WAYToken is ERC20, Ownable {
+    constructor() ERC20("WAY Token", "WAY") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
