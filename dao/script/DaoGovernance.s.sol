a58d9d329dd0837920a300f4397260b4a3800fb9
znan2 <previouslyon25@gmail.com>
2025-03-02 11:51:42 +0900

    chore:solidity version change

--- dao/script/DaoGovernance.s.sol
+++ dao/script/DaoGovernance.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "forge-std/Script.sol";
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
@@ -18,14 +18,14 @@ contract DaoGovernanceScript is Script {
 
     function run() public {
         uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-
+        address deployer = vm.addr(deployerPrivateKey);
         vm.startBroadcast();
         DaoGovernanceV1 daoV1 = new DaoGovernanceV1();
         WAYToken token = new WAYToken();
         bytes memory initDataV1 = abi.encodeWithSelector(daoV1.initialize.selector, 
             ERC20Upgradeable(address(token))
         );
-
+        //프록시
         DaoProxy proxy = new DaoProxy(address(daoV1), initDataV1);
         DaoGovernanceV1 governance = DaoGovernanceV1(address(proxy));
 
@@ -34,10 +34,40 @@ contract DaoGovernanceScript is Script {
             ERC20Upgradeable(address(token))
         );
         governance.approveUpgrade();
+        //V2업그레이드
         governance.upgradeImplementation(address(daoV2), initDataV2);
         DaoGovernanceV2 governanceV2 = DaoGovernanceV2(address(proxy));
-        string memory ver = governanceV2.version();
-        console.log(ver);
+        string memory ver2 = governanceV2.version();
+        console.log(ver2);
+
+        DaoGovernanceV3 daoV3 = new DaoGovernanceV3();
+        bytes memory initDataV3 = abi.encodeWithSelector(
+            DaoGovernanceV3.initializeV3.selector, 
+            ERC20Upgradeable(address(token))
+        );
+        governanceV2.approveUpgrade();
+        //V3업그레이드
+        governanceV2.upgradeImplementation(address(daoV3), initDataV3);
+        DaoGovernanceV3 governanceV3 = DaoGovernanceV3(address(proxy));
+        string memory ver3 = governanceV3.version();
+        console.log(ver3);
+
+        //스테이킹
+        uint256 stakeAmount = 100 ether;
+        token.approve(address(governanceV3), stakeAmount);
+
+        console.log("Deployer balance before staking:", token.balanceOf(deployer));
+        governanceV3.stake(stakeAmount);
+        console.log("Deployer balance after staking:", token.balanceOf(deployer));
+        //7일 뒤에 unstaking
+        uint256 currentTime = block.timestamp;
+        vm.warp(currentTime + 7 days + 1);
+        uint256 unstakeAmount = stakeAmount / 2;
+        governanceV3.unstake(unstakeAmount);
+        console.log("Deployer balance after unstaking:", token.balanceOf(deployer));
+        //출금
+        governanceV3.withdraw();
+        console.log("Deployer balance after withdraw:", token.balanceOf(deployer));
         vm.stopBroadcast();
     }
 }
--- dao/src/WAYToken.sol
+++ dao/src/WAYToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
